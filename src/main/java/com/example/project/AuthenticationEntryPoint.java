04aee32e92636f9503c97d3541aac2c65e2518cf
User <user.push@hackerrank.workspace>
2022-01-15 10:35:43 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/example/project/AuthenticationEntryPoint.java
+++ src/main/java/com/example/project/AuthenticationEntryPoint.java
@@ -8,12 +8,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
-	@Override
+	// @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
       throws IOException, ServletException {
         response.addHeader("LoginUser", "Basic " +getRealmName());
