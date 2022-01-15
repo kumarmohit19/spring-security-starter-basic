e113ce0a53238329b30bc83ab3ab71dbec918ada
User <user.push@hackerrank.workspace>
2022-01-15 10:56:33 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/example/project/AuthenticationEntryPoint.java
+++ src/main/java/com/example/project/AuthenticationEntryPoint.java
@@ -8,18 +8,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
-	// @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-      throws IOException, ServletException {
-        response.addHeader("LoginUser", "Basic " +getRealmName());
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        PrintWriter writer = response.getWriter();
-        writer.println("HTTP Status 401 - " + authEx.getMessage());
-    }
+	@Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+    throws IOException, ServletException {
+      response.addHeader("LoginUser", "Basic " +getRealmName());
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      PrintWriter writer = response.getWriter();
+      writer.println("HTTP Status 401 - " + authEx.getMessage());
+  }
 	@Override
     public void afterPropertiesSet() throws Exception {
         setRealmName("springboot");
